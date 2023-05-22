505c59679dc09a6f732a67127a67b3bd6c542a4e
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2023-05-22 21:56:04 +0700

    add encrypt and decrypt

--- app/page.js
+++ app/page.js
@@ -1,113 +1,153 @@
-import Image from 'next/image'
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import rsa from 'js-crypto-rsa'
 
 export default function Home() {
+  const [password, setPassword] = useState("");
+  const [publicKey, setPublicKey] = useState(null);
+  const [privateKey, setPrivateKey] = useState(null);
+  const [isGenerated, setIsGenerated] = useState(false);
+  const [key, setKey] = useState("");
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+  const [isEncrypt, setIsEncrypt] = useState(true);
+  const [n, setN] = useState("");
+
+  // enter password as the key
+  const generateKeys = () => {
+    rsa.generateKey(2048).then((keypair) => {
+      setPublicKey(keypair.publicKey);
+      setPrivateKey(keypair.privateKey);
+      setN(keypair.publicKey.n.toString(16));
+      setKey(keypair.publicKey.e.toString(16));
+      setIsGenerated(true);
+    });
+  };
+
+  const process = () => {
+    // if (isEncrypt) {
+    //   // convert key from hex to decimal
+    //   const e = BigInt("0x" + key);
+    //   const n = BigInt("0x" + n);
+    //   publicKey.e = e;
+    //   publicKey.n = n;
+
+    //   encrypt();
+    // } else {
+    //   // convert key from hex to decimal
+    //   const d = BigInt("0x" + key);
+    //   const n = BigInt("0x" + n);
+    //   privateKey.d = d;
+    //   privateKey.n = n;
+
+    //   decrypt();
+    // }
+  };
+
+  const encrypt = () => {
+    const encrypted = publicKey.encrypt(text);
+    setResult(encrypted);
+  };
+
+  const decrypt = () => {
+    const decrypted = privateKey.decrypt(text);
+    setResult(decrypted);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="w-[50%]">
+      <h1 className="text-3xl font-semibold">Notion RSA</h1>
+
+      <button type="button" className="p-4 border-2" onClick={generateKeys}>
+        Generate Keys
+      </button>
+      <div className={isGenerated ? "pt-6 break-words" : "hidden"}>
+        <h2 className="text-xl font-medium">Public Key</h2>
+        <p>Use this for encryption:</p>
+        <p>Key: {isGenerated ? publicKey.e.toString(16) : ""}</p>
+        <p>N: {n}</p>
+        <h2 className="text-xl font-medium">Private Key</h2>
+        <p>Use this for decryption [KEEP IT SECRET!]:</p>
+        <p>Key: {isGenerated ? privateKey.d.toString(16) : ""}</p>
+        <p>N: {n}</p>
+      </div>
+      <div className="pt-6">
+        <h2 className="text-xl font-medium">Enter Keys and Text</h2>
+        <div className="flex gap-3">
+          <div>
+            <label>Encryption/Decryption Key: </label>
+            <input
+              value={key}
+              onChange={(e) => setKey(e.target.value)}
+              type="text"
+              className="border-2"
             />
-          </a>
+          </div>
+          <div>
+            <label>N: </label>
+            <input
+              value={n}
+              onChange={(e) => setN(e.target.value)}
+              type="text"
+              className="border-2"
+            />
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="pt-6">
+        <label>Text to Encrypt/Decrypt: </label>
+        <div>
+          <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            rows="10"
+            className="border-2 w-full"
+          ></textarea>
+        </div>
+      </div>
+
+      <div className="pt-6">
+        <h2 className="text-xl font-medium">Select Operation</h2>
+        <input
+          type="radio"
+          name="option"
+          id="encrypt"
+          onChange={() => setIsEncrypt(true)}
+          checked={isEncrypt}
         />
+        <label> Encrypt Text </label>
+        <input
+          type="radio"
+          name="option"
+          id="decrypt"
+          onChange={() => setIsEncrypt(false)}
+          checked={!isEncrypt}
+        />
+        <label> Decrypt Text </label>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="pt-6">
+        <button
+          type="button"
+          onClick={() => process()}
+          className="p-4 border-2"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
+          Generate Text
+        </button>
+        <button
+          type="button"
+          className="p-4 border-2"
+          onClick={() => navigator.clipboard.writeText(result)}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Copy to clipboard
+        </button>
       </div>
+
+      <h1 className="pt-6 text-xl font-medium">Result:</h1>
+      <div>{result}</div>
     </main>
-  )
+  );
 }
